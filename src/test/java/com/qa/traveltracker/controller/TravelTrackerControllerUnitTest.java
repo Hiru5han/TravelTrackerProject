71d6b651b5158341b7529dce53e5c7a3d44790ec
Hiru5han <hirushan@me.com>
2021-10-06 11:38:05 +0100

    started testing the controller

---
+++ src/test/java/com/qa/traveltracker/controller/TravelTrackerControllerUnitTest.java
@@ -0,0 +1,14 @@
+package com.qa.traveltracker.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TravelTrackerControllerUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/test/java/com/qa/traveltracker/service/TrackerServiceUnitTest.java
+++ src/test/java/com/qa/traveltracker/service/TrackerServiceUnitTest.java
@@ -1,4 +1,4 @@
-package com.qa.traveltracker;
+package com.qa.traveltracker.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
